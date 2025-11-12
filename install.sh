b6deb9917c03dc621e1a39b2fa1197c6fa0f8e38
NekoBarry <alex@heuger.net>
2025-11-12 21:13:10 +0100

    impala for wifi; btop for taskmanager;

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 echo "Installing all dependencies"
 pkg_hypr="hyprland hyprlock hyprpaper hypridle waybar ly"
 pkg_term="alacritty kitty wofi rofi yazi lazygit lazydocker git starship zsh"
-pkg_quality="eza dunst fastfetch wiremix"
-pkg_network="discord telegram-desktop firefox"
+pkg_quality="eza dunst fastfetch wiremix btop"
+pkg_network="discord telegram-desktop firefox impala"
+pkg_unused_for_now="blueman"
 
 sudo pacman -Syu --needed $pkg_hypr $pkg_term $pkg_quality $pkg_network
 
