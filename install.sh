4075a71b584fc6339e001097a5458e589e7fc995
NekoBarry <alex@heuger.net>
2025-11-16 14:50:35 +0100

    dunstrc fix; dependencies;

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing all dependencies"
 pkg_hypr="hyprland hyprlock hyprpaper hypridle waybar ly uwsm"
 pkg_term="alacritty kitty wofi rofi yazi lazygit lazydocker git starship zsh ttf-jetbrains-mono-nerd"
-pkg_quality="eza dunst fastfetch wiremix btop"
+pkg_quality="eza dunst fastfetch wiremix btop nwg-displays nwg-look"
 pkg_messanger="discord telegram-desktop elecwhat"
 pkg_network="networkmanager nm-connection-editor"
 pkg_unused_for_now="blueman"
@@ -17,7 +17,7 @@ cd ..
 rm -rf paru
 
 #Installing Paru dependend packages
-paru -S --needed hyprnotify grimblast metar $pkg_messanger
+paru -S --needed grimblast metar $pkg_messanger
 
 #Copy config files
 sh copyConfig.sh
